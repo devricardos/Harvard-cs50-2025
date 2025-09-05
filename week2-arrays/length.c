29e752ad8d3d688e9920692d8b9417e02ef1f359
Ricardo Delfino <rickysolver@gmail.com>
2025-09-05 17:55:34 -0300

    Update length.c with string.h libary and strlen function

--- week2-arrays/length.c
+++ week2-arrays/length.c
@@ -1,5 +1,6 @@
 #include <cs50.h>  // Include CS50 library for input/output
 #include <stdio.h> // Include standard input/output library
+#include <string.h> // Included now to use the strlen function
 
 // Function prototype to calculate string length
 int string_length(string s);
@@ -7,16 +8,6 @@ int string_length(string s);
 int main(void)
 {
     string name = get_string("Enter a Name: "); // Get input string from user
-    int length = string_length(name);   // Call function to get length of the string
+    int length = strlen(name);   // Call function strlen that comes with <string.h>
     printf("%i\n", length);             // Print the length of the string
-}
-
-int string_length(string s)
-{
-    int n = 0;           // Initialize counter for string length
-    while (s[n] != '\0') // Loop until null terminator is reached
-    {
-        n++; // Increment counter for each character
-    }
-    return n; // Return the total length
 }
