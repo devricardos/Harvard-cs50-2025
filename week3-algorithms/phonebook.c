c9ee0e79fb5031b9d665f3ca493ff939e12f26b7
Ricardo Delfino <rickysolver@gmail.com>
2025-09-09 07:35:23 -0300

    refactor: phonebook.c creating a struct called person

--- week3-algorithms/phonebook.c
+++ week3-algorithms/phonebook.c
@@ -1,26 +1,40 @@
-// Implements a phone book without structs
+// Implements a phone book with structs
 // CS50x 2025 - Lecture 3 - Algorithms
-// https://youtu.be/iCx3zwK8Ms8?list=PLhQjrBD2T383q7Vn8QnTsVgSvyLpsqL_R&t=3003
+// https://youtu.be/iCx3zwK8Ms8?list=PLhQjrBD2T383q7Vn8QnTsVgSvyLpsqL_R&t=3410
 
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
+// create a struct called person, with name and number inside.
+typedef struct
+{
+    string name;
+    string number;
+} person;
+
 int main(void)
 {
-    // Arrays of strings name and numbers
-    string names[] = {"Yuliia", "David", "John"};
-    string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+    person people[3];
+
+    // New operator "."
+    people[0].name = "Yuliia";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-617-495-1000";
+
+    people[2].name = "John";
+    people[2].number = "+1-949-468-2750";
 
     // Search for name
     string name = get_string("Name: ");
     for (int i = 0; i < 3; i++)
     {
         // strcmp from string.h to compare strings, inputed name vs names array
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
-            // Return the number from the same location of the name array
-            printf("Found %s\n", numbers[i]);
+            printf("Found number %s\n", people[i].number);
             return 0; // Success
         }
     }
