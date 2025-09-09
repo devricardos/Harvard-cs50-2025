aff456e9ba736e96d91fbae791a9a69c9b37e593
Ricardo Delfino <rickysolver@gmail.com>
2025-09-08 22:44:26 -0300

    update search.c to implement linear search for strings with strcmp

--- week3-algorithms/search.c
+++ week3-algorithms/search.c
@@ -1,23 +1,25 @@
-// Implements linear search for integers - just as lockers sample
+// Revamp the code to implement linear search for strings
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    // An array of integers, no need to set its length
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    // An array of strings (with text now)
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    // Search for number "7" fixed loops (magic number)
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
+    // Search for string
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i++)
     {
-        if (numbers[i] == n)
+        // Where magic happens: strcmp to compare strings from string.h
+        if (strcmp(strings[i], s) == 0)
         {
             printf("Found\n");
-            return 0; // Exit Success
+            return 0; // Success
         }
     }
     printf("Not found\n");
-    return 1; // Exit Failure
+    return 1; // Error
 }
