92d057d27e19ead4a47c2737b5b733a6a5c6f666
Ricardo Delfino <rickysolver@gmail.com>
2025-09-20 22:49:16 -0300

    update list.c implements a sorted linked list of numbers using nodes & pointers

--- week5-data-structures/list.c
+++ week5-data-structures/list.c
@@ -1,54 +1,107 @@
-// This program demonstrates how to create a linked list in C by prepending nodes.
-// A linked list is a data structure where each element (node) contains data and a pointer to the next node.
+// Implements a sorted linked list of numbers
+// https://cs50.harvard.edu/x/weeks/5/
+// https://youtu.be/aV8LlSmd1E8?t=4767
 
-// Include necessary libraries
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node in the linked list
-// A node contains an integer (number) and a pointer to the next node
+// Define a structure called 'node' to represent each element in a linked list
 typedef struct node
 {
-    int number;          // The data stored in this node (an integer)
-    struct node *next;   // Pointer to the next node in the list
-} node;                  // Name of the structure is 'node'
+    int number;         // Stores an integer value
+    struct node *next;  // Pointer to the next node in the list
+} node;
 
-// The main function is where the program starts
+// Declare the unload function to free memory used by the linked list
+void unload(node *list);
+
+// Main function where the program starts
 int main(void)
 {
-    // Step 1: Initialize an empty linked list
-    // 'list' is a pointer to the first node; set to NULL since the list is empty
+    // Create a pointer to the start of the linked list, initially set to NULL (empty list)
     node *list = NULL;
 
-    // Step 2: Loop to create 3 nodes and add them to the list
-    // We'll ask the user for 3 numbers and prepend each as a new node
+    // Loop to get 3 numbers from the user to build the linked list
     for (int i = 0; i < 3; i++)
     {
-        // Step 3: Allocate memory for a new node
-        // malloc reserves space for one node (size of the node structure)
+        // Allocate memory for a new node
         node *n = malloc(sizeof(node));
-
-        // Step 4: Check if memory allocation failed
+        // Check if memory allocation failed
         if (n == NULL)
         {
-            // Exit the program with error code 1
+            // If allocation fails, free the existing list and exit
+            unload(list);
             return 1;
         }
-
-        // Step 5: Initialize the new node
-        // Ask the user for a number and store it in the node's 'number' field
+        // Get a number from the user and store it in the node's number field
         n->number = get_int("Number: ");
-        // Set the node's 'next' pointer to NULL (no next node yet)
+        // Set the new node's next pointer to NULL (it doesn't point to anything yet)
         n->next = NULL;
 
-        // Step 6: Prepend the new node to the list
-        // Make the new node's 'next' pointer point to the current head of the list
-        n->next = list;
-        // Update the head of the list to point to the new node
-        list = n;
+        // If the list is empty, make the new node the first node in the list
+        if (list == NULL)
+        {
+            list = n;
+        }
+        // If the new number is smaller than the first number in the list
+        else if (n->number < list->number)
+        {
+            // Make the new node point to the current list
+            n->next = list;
+            // Update the list to start with the new node
+            list = n;
+        }
+        // If the new number belongs somewhere else in the list
+        else
+        {
+            // Loop through the list to find the right place for the new node
+            for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                // If we're at the end of the list
+                if (ptr->next == NULL)
+                {
+                    // Add the new node to the end
+                    ptr->next = n;
+                    break; // Exit the loop since we added the node
+                }
+                // If the new number is smaller than the next node's number
+                if (n->number < ptr->next->number)
+                {
+                    // Insert the new node between the current node and the next node
+                    n->next = ptr->next;
+                    ptr->next = n;
+                    break; // Exit the loop since we added the node
+                }
+            }
+        }
     }
 
-    // Step 7: Return 0 to indicate the program ran successfully
+    // Print all numbers in the list, they will be printed in ascending order
+    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+    {
+        // Print the number stored in the current node
+        printf("%i\n", ptr->number);
+    }
+
+    // Free the memory used by the linked list
+    unload(list);
     return 0;
+}
+
+// Function to free the memory used by the linked list
+void unload(node *list)
+{
+    // Start with the first node
+    node *ptr = list;
+    // Keep going until we reach the end of the list
+    while (ptr != NULL)
+    {
+        // Save the pointer to the next node
+        node *next = ptr->next;
+        // Free the current node's memory
+        free(ptr);
+        // Move to the next node
+        ptr = next;
+    }
 }
